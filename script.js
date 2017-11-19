4cadd1fea26224ce2d9193c0e0ace0dd5b78b76c
Irina <irbris@mail.ru>
2017-11-19 15:36:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var container = document.querySelector(".parallax");
+var layers = document.querySelectorAll(".layer");
+
+function shadow(event) {
+    var width = container.offsetWidth;
+    var height = container.offsetHeight;
+    
+    var walk = 80; // 80px
+    
+    var x = event.offsetX;
+    var y = event.offsetY;
+    var z = 0;
+
+    var xWalk = Math.round((x / width * walk) - (walk / 2));
+    var yWalk = Math.round((y / height * walk) - (walk / 2));
+    
+    layers.forEach(function(layer, index) {
+        layer.style.webkitTransform = `
+translate3d(${xWalk*index/15}px, ${yWalk*index/20}px, ${z}px)`; 
+    })
+    
+}
+
+container.addEventListener("mousemove", shadow);
